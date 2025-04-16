ea632febf5d449cd072716247b63c734fd196ef1
Rohit kumar <sahrohitkumar10@gmail.com>
2025-04-17 02:32:12 +0530

    Update vite.config.js
    deployment url updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -19,10 +19,22 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://authfeedback-backend.onrender.com',
         changeOrigin: true,
-        secure: false,
-      },
+        secure: true,
+        ws: true,
+        configure: (proxy, _options) => {
+          proxy.on('error', (err, _req, _res) => {
+            console.log('proxy error', err);
+          });
+          proxy.on('proxyReq', (proxyReq, req, _res) => {
+            console.log('Sending Request:', req.method, req.url);
+          });
+          proxy.on('proxyRes', (proxyRes, req, _res) => {
+            console.log('Received Response:', proxyRes.statusCode, req.url);
+          });
+        }
+      }
     },
   },
   build: {
