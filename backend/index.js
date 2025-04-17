03ac06ce6f7d0cd3e153d43d2de3dc80dff24286
Rohit kumar <sahrohitkumar10@gmail.com>
2025-04-18 03:22:03 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(cookieParser());
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://authfeedback-frontend.onrender.com'
+    'https://auth-feedback-sysytem-qkq8.vercel.app'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
